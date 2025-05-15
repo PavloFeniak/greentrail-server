15684871b4bca4eac5162cc4737919854cf44e47
PavloFeniak <phenyakpablo@gmail.com>
2025-05-15 23:20:24 +0300

    feat (gateway): implement Jwt authentication for routing

---
+++ api-gateway/src/main/java/org/example/apigateway/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package org.example.apigateway.config;
+
+import org.example.apigateway.JWT.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/user/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-service"))
+                .route("auth-service", r -> r.path("/auth/**")
+                        .uri("lb://auth-service"))
+                .route("eclient", r -> r.path("/eclient/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://eclient-service"))
+
+                .build();
+    }
+}
