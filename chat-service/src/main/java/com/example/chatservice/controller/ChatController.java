be9393e09ddf4855cb79d455449fe2837d032c7f
PavloFeniak <phenyakpablo@gmail.com>
2025-05-16 23:51:13 +0300

    feat (chat): add chat-service endpoints

--- chat-service/src/main/java/com/example/chatservice/controller/ChatController.java
+++ chat-service/src/main/java/com/example/chatservice/controller/ChatController.java
@@ -1,13 +1,55 @@
 package com.example.chatservice.controller;
 
-import com.example.chatservice.entity.model.Chat;
+import com.example.chatservice.entity.DTO.ChatRequestDTO;
+import com.example.chatservice.entity.DTO.ChatResponseDTO;
+import com.example.chatservice.service.ChatService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/chat")
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final ChatService chatService;
     @PostMapping
-    public Chat getChat(@RequestBody Chat chat) {
-        return chat;
+    public ResponseEntity<?> createChat(@Valid @RequestBody ChatRequestDTO chatRequestDTO) {
+        try {
+            ChatResponseDTO createdChat = chatService.createChat(chatRequestDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdChat);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/trip/{tripId}")
+    public ResponseEntity<?> getChatByTripId(@PathVariable Long tripId) {
+        try {
+            ChatResponseDTO chat = chatService.getChatByTripId(tripId);
+            return ResponseEntity.ok(chat);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getChatById(@PathVariable Long id) {
+        try {
+            ChatResponseDTO chat = chatService.getChatById(id);
+            return ResponseEntity.ok(chat);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChatResponseDTO>> getAllChats() {
+        return ResponseEntity.ok(chatService.getAllChats());
     }
 }
---
+++ chat-service/src/main/java/com/example/chatservice/controller/ChatMessageController.java
@@ -0,0 +1,41 @@
+package com.example.chatservice.controller;
+
+import com.example.chatservice.entity.DTO.ChatMessageRequestDTO;
+import com.example.chatservice.entity.DTO.ChatMessageResponseDTO;
+import com.example.chatservice.service.ChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/chat-messages")
+@RequiredArgsConstructor
+public class ChatMessageController {
+
+    private final ChatMessageService chatMessageService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody ChatMessageRequestDTO chatMessageRequestDTO) {
+        try{
+            ChatMessageResponseDTO chatMessageResponseDTO = chatMessageService.sendMessage(chatMessageRequestDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(chatMessageResponseDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+    @GetMapping("/chat/{chatId}")
+    public ResponseEntity<?> getMessagesInChat(@PathVariable Long chatId) {
+        try {
+            List<ChatMessageResponseDTO> messages = chatMessageService.getMessagesInChat(chatId);
+            return ResponseEntity.ok(messages);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+        }
+    }
+
+}
