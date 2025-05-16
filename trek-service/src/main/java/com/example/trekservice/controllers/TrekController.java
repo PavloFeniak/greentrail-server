5f26e5bef2ec6d95b47a6fdad087f9825994335b
PavloFeniak <phenyakpablo@gmail.com>
2025-05-16 23:10:45 +0300

    feat (trek): create trek-service endpoints

--- trek-service/src/main/java/com/example/trekservice/controllers/TrekController.java
+++ trek-service/src/main/java/com/example/trekservice/controllers/TrekController.java
@@ -1,15 +1,54 @@
 package com.example.trekservice.controllers;
 
+import com.example.trekservice.entity.DTO.TrekRequestDto;
+import com.example.trekservice.entity.DTO.TrekResponseDto;
 import com.example.trekservice.entity.models.Treks;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.trekservice.services.TrekService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/treks")
+@RequiredArgsConstructor
 public class TrekController {
+
+    private final TrekService trekService;
+
     @PostMapping
-    public Treks getTreks(Treks treks) {
-        return treks;
+    public ResponseEntity<TrekResponseDto> createTrek(@RequestBody TrekRequestDto trekRequestDto) {
+        TrekResponseDto treks =  trekService.createTrek(trekRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(treks);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrekResponseDto> getTrekBuId(@PathVariable Long id) {
+        try {
+            TrekResponseDto trek = trekService.getTrekById(id);
+            return ResponseEntity.ok(trek);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TrekResponseDto>> getAllTreks() {
+        List<TrekResponseDto> treks = trekService.getAllTreks();
+        if (treks.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(treks);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTrek(@PathVariable Long id) {
+        trekService.deleteTrek(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
