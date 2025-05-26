8386a5024c5e187b3b5efc11dffa0594488e270a
PavloFeniak <phenyakpablo@gmail.com>
2025-05-26 21:39:50 +0300

    feat: create kind cluster

---
+++ config/build_and_push.sh
@@ -0,0 +1,18 @@
+
+SERVICE_NAME=$1
+IMAGE_TAG=$2
+
+cp config/Dockerfile $SERVICE_NAME/Dockerfile
+cd $SERVICE_NAME || exit 1
+
+./gradlew clean build
+
+docker build . -t $SERVICE_NAME:$IMAGE_TAG
+kind load docker-image $SERVICE_NAME:$IMAGE_TAG
+
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+
+kubectl get pods -w
+
+rm Dockerfile
